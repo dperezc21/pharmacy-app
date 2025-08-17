66fd14f8d149a6910a230cfc0361986f3b85b2f6
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-08-16 21:44:05 -0500

    feat: using electron to execute angular project like desktop app

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      contextIsolation: true
+    }
+  });
+
+  // Carga el index.html compilado por Angular
+  win.loadFile(path.join(__dirname, 'dist/pharmacy-app/browser/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
