81b84e4febb1805c90b8fd08e1d61a9abd3b46b1
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-08-23 16:35:36 -0500

    feat: inventory service file

---
+++ src/app/services/inventory.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map, Observable} from 'rxjs';
+import {INVENTORY_URL} from '../constants/url.constants';
+
+@Injectable({ providedIn: "root"})
+export class InventoryService {
+  constructor(private http: HttpClient) {}
+
+  verifyProductInInventory(productId: number, quantityInStock: number): Observable<boolean> {
+    return this.http.get<boolean>(`${INVENTORY_URL}/${productId}/${quantityInStock}`)
+                    .pipe(map(value => value));
+  }
+}
