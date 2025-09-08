7460e9e9534ee72383fb70e1cd7c54269de15fd9
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-09-07 23:27:44 -0500

    feat: add paginator to inventory list

--- src/app/components/inventory-list/inventory-list.component.ts
+++ src/app/components/inventory-list/inventory-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import {AfterViewInit, Component, computed, input, signal, ViewChild} from '@angular/core';
 import {DatePipe, NgClass} from '@angular/common';
 import {
   MatCell,
@@ -10,6 +10,7 @@ import {
   MatRow, MatRowDef, MatTable
 } from '@angular/material/table';
 import {InventoryProduct} from '../../models/product.model';
+import {MatPaginator, PageEvent} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-inventory-list',
@@ -25,15 +26,26 @@ import {InventoryProduct} from '../../models/product.model';
     MatRowDef,
     MatTable,
     NgClass,
-    MatHeaderCellDef
+    MatHeaderCellDef,
+    MatPaginator,
+    MatPaginator
   ],
   templateUrl: './inventory-list.component.html',
   standalone: true,
   styleUrl: './inventory-list.component.css'
 })
-export class InventoryListComponent {
+export class InventoryListComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   products = input<InventoryProduct[]>([]);
   displayedColumns: string[] = ['code', 'name', 'stock', 'purchaseDate', 'stockLevel'];
+
+  private pageIndex = signal(0);
+  private pageSize = signal(5);
+
+  protected itemsPaginated = computed(() => {
+    const start = this.pageIndex() * this.pageSize();
+    return this.products().slice(start, start + this.pageSize());
+  });
 
   getStockLevelText(stock: number): string {
     if (stock === 0) return 'Sin stock';
@@ -47,6 +59,18 @@ export class InventoryListComponent {
     if (stock <= 5) return 'stock-low';
     if (stock <= 10) return 'stock-medium';
     return 'stock-high';
+  }
+
+  changePage($event: PageEvent) {
+    this.pageIndex.set($event.pageIndex);
+    this.pageSize.set($event.pageSize);
+  }
+
+  ngAfterViewInit(): void {
+    this.paginator?.page?.subscribe(event => {
+      this.pageIndex.set(event.pageIndex);
+      this.pageSize.set(event.pageSize);
+    });
   }
 
 }
