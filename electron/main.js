5c52fa5fde213a5c3c1834eca8d93c1dcf528b25
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-09-12 10:19:52 -0500

    feat: replace file main electron

--- electron/main.js
+++ electron/main.js
@@ -3,22 +3,28 @@ const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1024,
-    height: 768,
+    width: 1200,
+    height: 800,
     webPreferences: {
       /*contextIsolation: true*/
-      contextIsolation: false,   // ❗ Cambiar esto
+      contextIsolation: true,   // ❗ Cambiar esto
       nodeIntegration: false,    // ❗ Desactiva esto para seguridad
-      webSecurity: false         // Solo si necesitas cargar recursos locales
+      webSecurity: true         // Solo si necesitas cargar recursos locales
     }
   });
 
   // Carga el index.html compilado por Angular
-  win.loadFile(path.join(__dirname, 'dist/pharmacy-app/browser/index.html'));
+  win.loadFile(path.join(__dirname, '../dist/pharmacy-app/browser/index.html'));
   //win.webContents.openDevTools(); // Para ver errores en consola
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
