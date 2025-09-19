4cf47b9f125e84dfbb938badcdbb63de30d4a191
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-09-18 23:23:02 -0500

    fix: cyclical dependence between controllers and services

--- src/app/controllers/user-authenticated.controller.ts
+++ src/app/controllers/user-authenticated.controller.ts
@@ -1,14 +1,10 @@
 import {computed, Injectable, Signal, signal} from '@angular/core';
 import {User} from '../models/user.models';
-import {AuthUserService} from '../services/auth-user.service';
-import {Observable, tap} from 'rxjs';
 
 @Injectable({ providedIn: "root" })
 export class UserAuthenticatedController {
   private _user = signal<User | null>(null);
   private _userToken = signal<string>("");
-
-  constructor(private authUserService: AuthUserService) {}
 
   private _userAuthenticated: Signal<User | null> = computed(() => this._user());
   private _userTokenGot: Signal<string> = computed(() => this._userToken());
@@ -32,11 +28,5 @@ export class UserAuthenticatedController {
 
   resetUserToken(): void {
     this._userToken.set("");
-  }
-
-  verifyTokenValid(): Observable<boolean> {
-    return this.authUserService.verifyToken().pipe(tap({
-      error: () => this._userToken.set("")
-    }));
   }
 }
--- src/app/guards/user-authenticated.guard.ts
+++ src/app/guards/user-authenticated.guard.ts
@@ -2,17 +2,22 @@ import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
 import {UserAuthenticatedController} from '../controllers/user-authenticated.controller';
 import {interval, switchMap, takeWhile, tap} from 'rxjs';
+import {AuthUserService} from '../services/auth-user.service';
 
 export const userAuthenticatedGuard: CanActivateFn = (route, state) => {
   const userAuthenticatedController: UserAuthenticatedController = inject(UserAuthenticatedController);
+  const authUserService: AuthUserService = inject(AuthUserService);
   const routerUrl: Router = inject(Router);
   const INTERVAL_TIME_ONE_HOUR: number = 1000 * 60;
 
   interval(INTERVAL_TIME_ONE_HOUR)
     .pipe(takeWhile(() => !!userAuthenticatedController.userTokenGot()),
     switchMap(() => {
-    return userAuthenticatedController.verifyTokenValid().pipe(tap({
-      error: () => routerUrl.navigate(['login'])
+    return authUserService.verifyToken().pipe(tap({
+      error: () => {
+        userAuthenticatedController.resetUserToken();
+        routerUrl.navigate(['login'])
+      }
     }))
   })).subscribe();
 
